f19cf0ebeb94a5d59fb1e27bb17b0e7e9644f22d
jcstill <jacobcstill@gmail.com>
2021-04-01 00:04:05 -0700

    add service the service updates the xen scheduling on boot also update the install script

--- install.sh
+++ install.sh
@@ -5,9 +5,14 @@ if [ "$EUID" -ne 0 ];then
 	exit 1
 fi
 
-install-v domtool /usr/bin/domtool
+install -v domtool /usr/bin/domtool
 
-mkdir -p /usr/local/man/man8
-install -g 0 -o 0 -m 0644 domtool.8 /usr/local/man/man8/
-gzip /usr/local/man/man8/domtool.8
+install -v -g 0 -o 0 -m 0640 domtool.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable domtool.service
+systemctl restart domtool.service
+
+mkdir -vp /usr/local/man/man8
+install -v -g 0 -o 0 -m 0644 domtool.8 /usr/local/man/man8/
+gzip -vf /usr/local/man/man8/domtool.8
 mandb
