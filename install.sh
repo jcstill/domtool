eb220e6ca4a4d0eb2ec3ef54252d0a3623b73ac6
jcstill <jacobcstill@gmail.com>
2021-03-31 23:47:27 -0700

    Add documentation and install script added a manpage (8) for domtool this fixes #6

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ];then
+	>&2 printf "\e[38;2;255;0;0m[!]\e[0m Please run as root\n"
+	exit 1
+fi
+
+install-v domtool /usr/bin/domtool
+
+mkdir -p /usr/local/man/man8
+install -g 0 -o 0 -m 0644 domtool.8 /usr/local/man/man8/
+gzip /usr/local/man/man8/domtool.8
+mandb
